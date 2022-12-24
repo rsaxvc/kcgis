0bc85856e271fa423fd83f35c0f736fac65960c8
Richard Allen <rsaxvc@rsaxvc.net>
2022-12-24 11:21:34 -0600

    update for 2022 and python3

--- download.py
+++ download.py
@@ -1,6 +1,6 @@
-#! /usr/bin/python
+#! /usr/bin/python3
 
-from Queue import Queue
+import queue
 from threading import Thread
 import requests
 import os
@@ -10,31 +10,32 @@ adapter = requests.adapters.HTTPAdapter(pool_connections=10, pool_maxsize=10)
 sess.mount('http://maps2.kcmo.org', adapter)
 
 def do_stuff(q):
-  while True:
-	(x,y) = q.get()
-	outpath = "files/"+str(x)+"_"+str(y)+".jpg"
-	if( not os.path.isfile(outpath) ):
-		item = "http://maps2.kcmo.org/kcgis/rest/services/external/Orthophoto_2016/MapServer/tile/10/"+str(x)+"/"+str(y)
-		try:
-			resp = sess.get(item,timeout=5)
-			if( resp.status_code == requests.codes.ok ):
-				file = open(outpath, "w")
-				file.write(resp.content)
-				file.close()
-				print "Downloaded "+outpath
-			else:
-				print "Failed to download "+str(x)+","+str(y)
-		except requests.exceptions.ConnectionError as errc:
-			print ("Error Connecting:",errc)
-		except requests.exceptions.Timeout:
-			print ("Timeout Error:",errt)
-		except requests.exceptions.RequestException as err:
-			print ("OOps: Something Else",err)
-	else:
-		print "Using cached "+outpath
-	q.task_done()
+	while True:
+		(x,y) = q.get()
+		outpath = "files/"+str(x)+"_"+str(y)+".jpg"
+		if( not os.path.isfile(outpath) ):
+			item = "http://maps2.kcmo.org/kcgis/rest/services/external/Orthophoto_2016/MapServer/tile/10/"+str(x)+"/"+str(y)
+			item = "https://mapd.kcmo.org/kcgis/rest/services/CachedMaps/Orthophoto_2022/MapServer/tile/10/"+str(x)+"/"+str(y)
+			try:
+				resp = sess.get(item,timeout=5)
+				if( resp.status_code == requests.codes.ok ):
+					file = open(outpath, "wb")
+					file.write(resp.content)
+					file.close()
+					print ("Downloaded "+outpath)
+				else:
+					print ("Failed to download "+str(x)+","+str(y))
+			except requests.exceptions.ConnectionError as errc:
+				print ("Error Connecting:",errc)
+			except requests.exceptions.Timeout:
+				print ("Timeout Error:",errt)
+			except requests.exceptions.RequestException as err:
+				print ("OOps: Something Else",err)
+		else:
+			print ("Using cached "+outpath)
+		q.task_done()
 
-q = Queue(maxsize=0)
+q = queue.Queue(maxsize=0)
 num_threads = 20
 
 for i in range(num_threads):
@@ -43,8 +44,8 @@ for i in range(num_threads):
 	worker.start()
 
 tiles = []
-for x in xrange(83309,84183):
-	for y in xrange(82656,83177):
+for x in range(83309,84183):
+	for y in range(82656,83177):
 		tiles.append( (x,y) )
 from random import shuffle
 shuffle(tiles)
--- reassemble.py
+++ reassemble.py
@@ -1,10 +1,11 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-from Queue import Queue
+import queue
 from threading import Thread
 import os
 import re
 import sys
+import psutil
 
 import argparse
 parser = argparse.ArgumentParser()
@@ -15,10 +16,10 @@ parser.add_argument('--maxlon', help='maximum longitude in block offsets')
 args = parser.parse_args()
 
 files = [f for f in os.listdir('./files') if os.path.isfile('./files/'+f)]
-bbox_lat_min =  sys.maxint
-bbox_lat_max = -sys.maxint
-bbox_lon_min =  sys.maxint
-bbox_lon_max = -sys.maxint
+bbox_lat_min =  sys.maxsize
+bbox_lat_max = -sys.maxsize
+bbox_lon_min =  sys.maxsize
+bbox_lon_max = -sys.maxsize
 
 for f in files:
 	match = re.search('([0-9]{5})_([0-9]{5}).jpg',f)
@@ -39,29 +40,28 @@ if args.maxlat != None:
 if args.maxlon != None:
 	bbox_lon_max = int(args.maxlon)
 
-print "BBOX LON:",bbox_lon_min,bbox_lon_max
-print "BBOX LAT:",bbox_lat_min,bbox_lat_max
+print ("BBOX LON:",bbox_lon_min,bbox_lon_max)
+print ("BBOX LAT:",bbox_lat_min,bbox_lat_max)
 
 from PIL import Image
 
 def do_stuff(q):
-  while True:
-	(f) = q.get()
-	match = re.search('([0-9]{5})_([0-9]{5}).jpg',f)
-	if match:
-		lat = int(match.group(1))-bbox_lat_min
-		lon = int(match.group(2))-bbox_lon_min
-		if lat >= 0 and lat <= bbox_lat_max - bbox_lat_min and lon >= 0 and lon <= bbox_lon_max - bbox_lon_min:
-			left  = rescale * lon
-			up    = rescale * lat
-			source = Image.open(f).convert("RGBA")
-			source.thumbnail((rescale,rescale))
-			im.paste(source,(left,up))
-			source.close();
-	q.task_done()
+	while True:
+		(f) = q.get()
+		match = re.search('([0-9]{5})_([0-9]{5}).jpg',f)
+		if match:
+			lat = int(match.group(1))-bbox_lat_min
+			lon = int(match.group(2))-bbox_lon_min
+			if lat >= 0 and lat <= bbox_lat_max - bbox_lat_min and lon >= 0 and lon <= bbox_lon_max - bbox_lon_min:
+				left  = rescale * lon
+				up    = rescale * lat
+				source = Image.open(f).convert("RGBA")
+				source.thumbnail((rescale,rescale))
+				im.paste(source,(left,up))
+				source.close();
+		q.task_done()
 
-import psutil
-q = Queue(maxsize=psutil.cpu_count())
+q = queue.Queue(maxsize=psutil.cpu_count())
 for i in range(psutil.cpu_count()):
 	worker = Thread(target=do_stuff, args=(q,))
 	worker.daemon = True
@@ -70,25 +70,28 @@ for i in range(psutil.cpu_count()):
 #Render image
 dlat = bbox_lat_max - bbox_lat_min + 1
 dlon = bbox_lon_max - bbox_lon_min + 1
-print "Source image is "+str(dlon*512)+"x"+str(dlat*512)
+print ("Source image is "+str(dlon*512)+"x"+str(dlat*512))
+#512x is the base tile resolution
+#However, the full city takes a lot of RAM to do,
+#so downscale each tile to width,height=rescale,rescale
 rescale = 64
 res_lat = rescale * dlat
 res_lon = rescale * dlon
 im = Image.new("RGB",(res_lon,res_lat))
 nFiles = len(files)
 threshold = 0.0
-for i in xrange(1,len(files)):
+for i in range(1,len(files)):
 	pct = float(i)/nFiles
 	if pct > threshold:
-		print 'Parsing Images ({0}%)\r'.format(int(100*pct)),
+		print ('Parsing Images ({0}%)\r'.format(int(100*pct)),)
 		sys.stdout.flush()
 		threshold += 0.01;
 	q.put( './files/'+files[i] )
 q.join()
 while( rescale > 0 ):
-	print "Rescaling to "+str(dlon*rescale)+"x"+str(dlat*rescale)
+	print ("Rescaling to "+str(dlon*rescale)+"x"+str(dlat*rescale))
 	im.save("bitmap_"+str(rescale)+".png")
-	rescale /= 2
+	rescale //= 2
 	sz = im.size
 	im.thumbnail((im.size[0]/2,im.size[1]/2))
 im.close()
